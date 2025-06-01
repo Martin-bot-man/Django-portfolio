1d4e46e34689acde1202b4f4d73bbf233f764fb1
Martin Odongo <martinwere14@gmail.com>
2025-06-01 16:32:12 +0300

    created sqlite database

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technologies = models.CharField(max_length=20)
+
 # Create your models here.
