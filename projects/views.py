7c3523b534cb03ce845d347c51e07c41e6c73f54
Martin Odongo <martinwere14@gmail.com>
2025-06-01 22:33:50 +0300

    rendering pages

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from projects.models import Project
 
-def Project_index(request):
+def project_index(request):
     projects = Project.objects.all()
     context ={
         "projects": projects
