c3d75ab14d71db52ce2b9a77b89e1445833e37b7
Martin Odongo <martinwere14@gmail.com>
2025-06-01 17:43:37 +0300

    created the database view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from projects.models import Project
+
+def Project_index(request):
+    projects = Project.objects.all()
+    context ={
+        "projects": projects
+    }
+    return render(request, "projects/project_index.html", context)
 
 # Create your views here.
